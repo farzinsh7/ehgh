7a3493a1b2b10d4899279f94d58194e2bad8c027
Your Namefarzinsh7 <you@example.com>
2023-10-01 13:40:03 +0330

    Make SocialLinks TabularInline

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import SiteInformation
+from .models import SiteInformation, SocialLinks
 
-admin.site.register(SiteInformation)
+
+class SocialLinksAdmin(admin.TabularInline):
+    model = SocialLinks
+    extra = 1
+    max_num = 10
+
+
+@admin.register(SiteInformation)
+class SiteInformationAdmin(admin.ModelAdmin):
+    inlines = [SocialLinksAdmin]
--- home/models.py
+++ home/models.py
@@ -8,10 +8,17 @@ class SiteInformation(models.Model):
     description = models.TextField(null=True)
     phone = models.CharField(max_length=11, null=True)
     address = models.CharField(max_length=300, null=True)
-    instagram = models.URLField(null=True)
-    telegram = models.URLField(null=True)
-    whatsapp = models.URLField(null=True)
     email = models.EmailField(null=True)
 
     def __str__(self):
         return self.title
+    
+
+class SocialLinks(models.Model):
+    label = models.CharField(max_length=120, null=True)
+    icon = models.CharField(null=True, max_length=200)
+    link = models.CharField(max_length=200,blank=True,null=True)
+    main_data = models.ForeignKey(SiteInformation, null=True, on_delete=models.SET_NULL, related_name='socials')
+
+    def __str__(self):
+        return self.label
