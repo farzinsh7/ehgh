d071169285ca8c2e740c9066831dbe798bebc2bc
farzinsh7 <farzinnater@gmail.com>
2023-10-03 17:49:44 +0330

    Create industry-detail-view

--- industries/urls.py
+++ industries/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndustryistView 
+from .views import IndustryistView, IndustryDetailView
 
 app_name = "industry"
 urlpatterns = [
     path('industries/', IndustryistView.as_view(), name="list_view"),
+    path('industries/<slug:slug>', IndustryDetailView.as_view(), name='detail_view'),
 ]
--- industries/views.py
+++ industries/views.py
@@ -10,3 +10,10 @@ class IndustryistView(ListView):
     template_name = 'industry_list.html'
     context_object_name = 'industry'
     # paginate_by = 1
+
+
+class IndustryDetailView(DetailView):
+    model = Industry
+    context_object_name = 'industry'
+    queryset = Industry.objects.filter(status='p')
+    template_name = 'industry_detail.html'
