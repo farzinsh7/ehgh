ed2b576f599501fe30735e813e27e48e1e371b02
farzinsh7 <farzinnater@gmail.com>
2023-10-14 22:38:47 +0330

    Create about view and create template

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import AboutView
 
 
 app_name = "about"
 urlpatterns = [
-    # path('about/', CompanyListView.as_view(), name = "page"),
+    path('about/', AboutView.as_view(), name = "page"),
 ]
