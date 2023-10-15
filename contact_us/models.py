d337b460c9b9cbcf5d69a46885951c88ec65513e
Your Namefarzinsh7 <you@example.com>
2023-10-15 15:03:50 +0330

    Create ContactUs & ContactForm Model

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ContactForm(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class ContactUS(models.Model):
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.title
