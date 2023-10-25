d0846e0e2f582de081da8a9b49eb8fda73745651
Your Namefarzinsh7 <you@example.com>
2023-10-25 13:59:49 +0330

    Dynamic HomeData & Information language by django-modeltranslation

---
+++ home/translation.py
@@ -0,0 +1,20 @@
+from modeltranslation.translator import TranslationOptions, register
+from .models import HomeData, Slider, Features, SiteInformation
+
+@register(HomeData)
+class HomeDataTranslationOptions(TranslationOptions):
+    fields = ['title', 'about', 'mission', 'quote', 'sub_industries']
+
+@register(Slider)
+class SliderTranslationOptions(TranslationOptions):
+    fields = ['title', 'description']
+
+
+@register(Features)
+class FeaturesTranslationOptions(TranslationOptions):
+    fields = ['title', 'description']
+
+
+@register(SiteInformation)
+class SiteInformationTranslationOptions(TranslationOptions):
+    fields = ['title', 'description', 'address']
