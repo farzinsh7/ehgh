2d94e21aa5e2ceeba51f4f0b85b9efd643dfa18c
farzinsh7 <farzinnater@gmail.com>
2023-11-02 23:15:06 +0330

    Add bg to login page

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 app_name = "account"
 urlpatterns = [
-    # path("login/", views.LoginView.as_view(), name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
     # path("logout/", views.LogoutView.as_view(), name="logout"),
     # path("password_change/", PasswordChange.as_view(), name="password_change"),
     # path("password_change/done/", views.PasswordChangeDoneView.as_view(), name="password_change_done",),
