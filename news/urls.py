92ec0d867a629990c79c35709aa1324cb01c9be4
farzinsh7 <farzinnater@gmail.com>
2024-06-09 10:41:11 +0330

    Create templates for news app

---
+++ news/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = "news"
+urlpatterns = [
+    # path('news/', NewsListView.as_view(), name="list_view"),
+    # path('news/<slug:slug>', NewsDetailView.as_view(), name='detail_view'),
+]
