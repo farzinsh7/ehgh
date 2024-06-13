73683ae2ce37b4788af2da6be62670f3422bb412
farzinsh7 <farzinnater@gmail.com>
2024-06-13 09:32:13 +0330

    Create URL For SiteInformation

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from home.views import IndexView
+from . import views
 
 app_name = "page"
 urlpatterns = [
-    path('', IndexView.as_view(), name="home"),
+    path('', views.IndexView.as_view(), name="home"),
+    path('header/', views.SiteHeaderView.as_view(), name="head"),
+    path('footer/', views.SiteFooterView.as_view(), name="footer"),
 ]
