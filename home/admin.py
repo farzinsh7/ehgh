52da561b83d351f2155b0f2df65b58ddea73a07a
farzinsh7 <farzinnater@gmail.com>
2024-06-13 09:24:35 +0330

    add Site Information to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class SocialLinksAdmin(admin.TabularInline):
+    model = models.SocialLinks
+    extra = 1
+    max_num = 10
+
+
+class HelpfulLinksAdmin(admin.TabularInline):
+    model = models.HelpfulLinks
+    extra = 1
+    max_num = 10
+
+
+@admin.register(models.SiteInformation)
+class SiteInformationAdmin(admin.ModelAdmin):
+    inlines = [SocialLinksAdmin, HelpfulLinksAdmin]
