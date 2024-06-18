f8091b612a5b1dff1efe20de1cd566abb055eed1
farzinsh7 <farzinnater@gmail.com>
2024-06-18 11:13:29 +0330

    Use Sorl_thumbnail for images in home page

--- brands/views.py
+++ brands/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Brands, Line, Tags
+from . import models
+from news.models import News
 
 
 # Create your views here.
 class BrandsListView(ListView):
-    model = Brands
-    queryset = Brands.objects.filter(status="p").order_by('-created')
+    model = models.Brands
+    queryset = models.Brands.objects.filter(status="p").order_by('-created')
     template_name = 'brands_list.html'
     context_object_name = 'brands'
 
 
 
 class BrandsDetailView(DetailView):
-    model = Brands
+    model = models.Brands
     context_object_name = 'brand'
-    queryset = Brands.objects.filter(status='p')
+    queryset = models.Brands.objects.filter(status='p')
     template_name = 'brands_detail.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['lines'] = Line.objects.filter(status=True)
-        context['tags'] = Tags.objects.filter(status=True)
-        context['latest'] = Brands.objects.filter(status="p").order_by('-created')[:4]
+        context['lines'] = models.Line.objects.filter(status=True)
+        context['tags'] = models.Tags.objects.filter(status=True)
+        context['latest'] = News.objects.filter(status="p").order_by('-publish')[:4]
         return context
