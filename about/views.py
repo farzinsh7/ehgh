7fbed1f95da56f7042fc141f20ca291c66fb13a7
farzinsh7 <farzinnater@gmail.com>
2024-06-18 14:52:48 +0330

    Add mvv to about page

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from . import models
+from home.models import SiteInformation
 
 # Create your views here.
 class AboutView(ListView):
@@ -8,3 +9,8 @@ class AboutView(ListView):
     queryset = models.AboutCompany.objects.first()
     template_name = 'about.html'
     context_object_name = "about"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = SiteInformation.objects.first()
+        return context
