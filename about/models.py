d0c951f4fd1441d0b09dfbdf8155e417f530bd6a
farzinsh7 <farzinnater@gmail.com>
2024-06-18 13:46:12 +0330

    Create about Model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+class AboutCompany(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="about")
+    description = HTMLField()
+    keywords = models.CharField(max_length=300, null=True)
+    seo_description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TeamMember(models.Model):
+    name = models.CharField(max_length=300, null=True)
+    position = models.CharField(max_length=300, null=True)
+    image = models.ImageField(upload_to="about/members", null=True)
+    about = models.ForeignKey(AboutCompany, null=True, on_delete=models.SET_NULL, related_name='members')
+    
+    def __str__(self):
+        return self.name
