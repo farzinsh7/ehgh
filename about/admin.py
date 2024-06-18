5a5d7b0f1f76e1f37ebcac487e8b273a3bec9a48
farzinsh7 <farzinnater@gmail.com>
2024-06-18 13:49:12 +0330

    Create about admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class TeamMemberAdmin(admin.TabularInline):
+    model = models.TeamMember
+    extra = 1
+    max_num = 10
+
+
+@admin.register(models.AboutCompany)
+class AboutCompanyAdmin(admin.ModelAdmin):
+    inlines = [TeamMemberAdmin]
