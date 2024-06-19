00b53a6683b18e2edaa158b87231e2b867662525
farzinsh7 <farzinnater@gmail.com>
2024-06-19 09:24:03 +0330

    add about to navbar

--- brands/models.py
+++ brands/models.py
@@ -51,7 +51,7 @@ class Brands(models.Model):
 
     def thumbnail_tag(self):
         return format_html(
-            "<img width=100 height=75 style='border-radius: 5px;' src='{}'>".format(self.image.url))
+            "<img width=100 height=75 style='border-radius: 5px;' src='{}'>".format(self.thumbnail.url))
         
 
 class Slider(models.Model):
