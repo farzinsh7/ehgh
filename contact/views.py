2b800919ce667c7414039d469b9421c92aff0f50
farzinsh7 <farzinnater@gmail.com>
2024-06-24 11:23:53 +0330

    Create forms and views

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import models
+from . import forms
 
-# Create your views here.
+
+class ContactUsView(CreateView):
+    model = models.ContactForm
+    form_class = forms.ContactFormClass
+    success_url = reverse_lazy('contact:page')
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact'] = models.ContactUS.objects.first()
+        return context
