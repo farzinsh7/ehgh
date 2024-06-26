dbcbd28fbb66b8403b5c8d4eb1b0d59cac7c3399
farzinsh7 <farzinnater@gmail.com>
2024-06-26 09:21:28 +0330

    Change the attrs of forms

--- contact/forms.py
+++ contact/forms.py
@@ -7,7 +7,7 @@ class ContactFormClass(forms.ModelForm):
         model = models.ContactForm
         fields = ('name', 'email', 'message')
         widgets = {
-        'name': forms.TextInput(attrs={'id': 'name', 'placeholder':'Name', 'type':'text'}),
-        'email': forms.EmailInput(attrs={'id': 'email', 'placeholder':'Email', 'type':'email'}),
-        'message': forms.Textarea(attrs={'id': 'message', 'placeholder':'Message'}),
+        'name': forms.TextInput(attrs={'placeholder':'Your Name', 'type':'text', 'class':'fs-0 form-control','required':'required'}),
+        'email': forms.EmailInput(attrs={'placeholder':'Email Address', 'type':'email', 'class':'fs-0 form-control','required':'required'}),
+        'message': forms.Textarea(attrs={'placeholder':'Type your message here', 'class':'fs-0 form-control contact-message', 'row':'8','required':'required'}),
         }
