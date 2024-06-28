ab3fa581792149f8e29988d40b307ce997a408ff
farzinsh7 <farzinnater@gmail.com>
2024-06-28 19:00:30 +0330

    Create houses model

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from tinymce.models import HTMLField
+
 
 # Create your models here.
+class Brands(models.Model):
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='houses')
+    description = HTMLField()
+    keywords = models.CharField(max_length=300, null=True)
+    seo_description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
