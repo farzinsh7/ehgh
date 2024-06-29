08dcf119838e98faaaab9f1c2757351d3dab3a3f
farzinsh7 <farzinnater@gmail.com>
2024-06-29 15:35:22 +0330

    Finish houses list_view

--- houses/urls.py
+++ houses/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "houses"
 urlpatterns = [
-    # path('houses/', views.HousesListView.as_view(), name="list_view"),
-    # path('houses/<slug:slug>', views.HousesDetailView.as_view(), name="detail_view"),
+    path('houses/', views.HousesListView.as_view(), name="list_view"),
+    path('houses/<slug:slug>', views.HousesDetailView.as_view(), name="detail_view"),
 ]
