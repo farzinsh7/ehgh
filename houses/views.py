8b8ab3c8ec32215d26d3c190955b8f2a626edf83
farzinsh7 <farzinnater@gmail.com>
2024-06-29 15:07:41 +0330

    Create houses views

--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . import models
 
 # Create your views here.
+class HousesListView(ListView):
+    model = models.Houses
+    queryset = models.Houses.objects.all()
+    template_name = 'houses_list.html'
+    context_object_name = 'houses'
+
+
+class HousesDetailView(DetailView):
+    model = models.Houses
+    context_object_name = 'house'
+    queryset = models.Houses.objects.all()
+    template_name = 'houses_detail.html'
+    
