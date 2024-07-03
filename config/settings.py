f9bb14145d47e18dd4d739c78a57ace7c4d9ab29
farzinsh7 <farzinnater@gmail.com>
2024-07-03 12:43:43 +0330

    Add new features to admin panel by jazzmin

--- config/settings.py
+++ config/settings.py
@@ -235,7 +235,31 @@ JAZZMIN_SETTINGS = {
 
     # Copyright on the footer
     "copyright": "Designed By Farzin",
+    
+    "usermenu_links": [
+        {"name": "Support", "url": "https://farzinshams.com", "new_window": True},
+        {"model": "auth.user"}
+    ],
 
+    "topmenu_links": [
+
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home",  "url": "page:home", "new_window": True},
+        {"name": "Brands",  "url": "brands:list_view", "new_window": True},
+        {"name": "Houses",  "url": "houses:list_view", "new_window": True},
+        {"name": "News",  "url": "news:list_view", "new_window": True},
+        {"name": "About",  "url": "about:page", "new_window": True},
+        {"name": "Contact",  "url": "contact:page", "new_window": True},
+
+        # external url that opens in a new window (Permissions can be added)
+        {"name": "Support", "url": "https://farzinshams.com", "new_window": True},
+
+        # model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
+
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "books"},
+    ],
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
     "icons": {
