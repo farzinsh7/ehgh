026b0af061e7d6bfdc3553207bd4f1fe4a24fdc4
farzinsh7 <farzinnater@gmail.com>
2024-08-04 12:59:28 +0330

    Fix db issue in about and home app

--- about/views.py
+++ about/views.py
@@ -5,9 +5,12 @@ from home.models import SiteInformation
 # Create your views here.
 class AboutView(ListView):
     model = models.AboutCompany
-    queryset = models.AboutCompany.objects.first()
     template_name = 'about.html'
     context_object_name = "about"
+    
+    
+    def get_queryset(self):
+        return models.AboutCompany.objects.first()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- home/views.py
+++ home/views.py
@@ -9,7 +9,9 @@ class IndexView(ListView):
     model = models.HomeData
     template_name = 'index.html'
     context_object_name = "home"
-    queryset = models.HomeData.objects.first()
+
+    def get_queryset(self):
+        return models.HomeData.objects.first()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,26 +20,28 @@ class IndexView(ListView):
         return context
 
 
-
 class SiteHeaderView(ListView):
     model = models.SiteInformation
     template_name = 'base/shared/header.html'
     context_object_name = 'info'
-    queryset = models.SiteInformation.objects.first()
-    
+
+    def get_queryset(self):
+        return models.SiteInformation.objects.first()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['houses'] = Houses.objects.all().order_by('position')
         return context
 
 
-
 class SiteFooterView(ListView):
     model = models.SiteInformation
     template_name = 'base/shared/footer.html'
     context_object_name = 'info'
-    queryset = models.SiteInformation.objects.first()
-    
+
+    def get_queryset(self):
+        return models.SiteInformation.objects.first()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest'] = News.objects.filter(status='p').order_by('-publish')[:3]
